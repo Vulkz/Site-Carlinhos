95699914b2320edb099115f35ea7f716aa7df98d
vulkz <gabrielcosta3536@gmail.com>
2023-12-08 01:16:02 -0300

    carrossel criado (precisa de melhorias)

---
+++ scripts/carrossel.js
@@ -0,0 +1,16 @@
+const imagens = document.getElementById('img');
+const imagem = document.getElementsByClassName('item');
+
+let idx = 0;
+
+function carrossel() {
+    idx++;
+
+    if(idx > imagem.length - 1){
+        idx = 0;
+    }
+
+    imagens.style.transform = `translateX(${-idx * 100}%)`;
+}
+
+setInterval(carrossel, 1800);
